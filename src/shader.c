4c5319841a15fffbc6e8f97139d292576c8d61a7
fjuras <fjuras@student.42wolfsburg.de>
2023-01-16 14:10:50 +0100

    Remove shadow check if the object is ocluded by itself

--- src/shader.c
+++ src/shader.c
@@ -6,7 +6,7 @@
 /*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 23:29:44 by fjuras            #+#    #+#             */
-/*   Updated: 2023/01/16 14:00:13 by fjuras           ###   ########.fr       */
+/*   Updated: 2023/01/16 14:08:00 by fjuras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,20 @@ double	shader(t_data *data, t_cast cast)
 	double	res;
 	t_v3	light;
 	double	light_dist;
+	double	light_dot_normal;
 	t_cast	shadow_cast;
 
 	res = data->scene->ambient->brightness;
 	light = v3_sub(data->scene->light->origin, cast.p);
 	light_dist = v3_len(light);
 	light = v3_mult(light, 1. / light_dist);
-	shadow_cast = intersection_except(data->scene->objects,
-		cast.obj, cast.p, light);
-	if (shadow_cast.obj == NULL || shadow_cast.t > light_dist)
-		res += ft_fmax(0., v3_dot(light, cast.n));
+	light_dot_normal = v3_dot(light, cast.n);
+	if (light_dot_normal > 0.)
+	{
+		shadow_cast = intersection_except(data->scene->objects,
+			cast.obj, cast.p, light);
+		if (shadow_cast.obj == NULL || shadow_cast.t > light_dist)
+			res += ft_fmax(0., light_dot_normal);
+	}
 	return (res);
 }
