d55456c24e9854dc4f2d0b1c15073ac0eb1b73e2
jkarosas <jkarosas@student.42wolfsburg.de>
2023-01-26 16:51:55 +0200

    1.00000000000000000000000001 fix

--- src/parse_lighting.c
+++ src/parse_lighting.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   parse_lighting.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: jkarosas <jkarosas@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:42:23 by jkarosas          #+#    #+#             */
-/*   Updated: 2023/01/24 19:12:01 by fjuras           ###   ########.fr       */
+/*   Updated: 2023/01/26 16:49:33 by jkarosas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+static int	brightness_check(char *nb)
+{
+	int	i;
+	
+	if (nb[0] == '1' && nb[1])
+		i = 2;
+	else if (nb[0] == '-' && nb[1] == '0')
+		i = 3;
+	else
+		i = -1;
+	while (nb[i])
+	{
+		if (nb[i] != '0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 int	parse_camera(t_scene *scene, char **line)
 {
@@ -46,7 +65,7 @@ int	parse_light(t_scene *scene, char **line)
 	light = malloc(sizeof(t_light));
 	error += parse_coordinates(&light->origin, line[1]);
 	light->brightness = ft_atof(line[2]);
-	if (light->brightness < 0.0 || light->brightness > 1.0)
+	if (light->brightness < 0.0 || light->brightness > 1.0 || brightness_check(line[2]))
 	{
 		free(light);
 		printf("Brightness ratio has to be in range [0.0,1.0]\n");
@@ -69,7 +88,7 @@ int	parse_ambient_lighting(t_scene *scene, char **line)
 		return (cleanup("Ambient light can only be declared once"));
 	alighting = malloc(sizeof(t_ambient));
 	alighting->brightness = ft_atof(line[1]);
-	if (alighting->brightness < 0.0 || alighting->brightness > 1.0)
+	if (alighting->brightness < 0.0 || alighting->brightness > 1.0 || brightness_check(line[1]))
 	{
 		free(alighting);
 		printf("Ambient light ratio has to be in range [0.0,1.0]\n");
--- src/parse_parameters.c
+++ src/parse_parameters.c
@@ -3,14 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   parse_parameters.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: jkarosas <jkarosas@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:06:11 by jkarosas          #+#    #+#             */
-/*   Updated: 2023/01/24 19:14:44 by fjuras           ###   ########.fr       */
+/*   Updated: 2023/01/26 16:29:34 by jkarosas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+static int	orientation_check(char **split)
+{
+	int	i;
+	int	y;
+
+	i = 0;
+	while (split[i])
+	{
+		if (split[i][0] == '1')
+			y = 2;
+		else if (split[i][0] == '-' && split[i][1] == '1')
+			y = 3;
+		else
+			y = -1;
+		while (split[i][y])
+		{
+			if (split[i][y] != '0')
+				return (1);
+			y++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	rgb_check(char **split)
+{
+	int	i;
+	int	y;
+	int	dec;
+	int	nul;
+
+	i = -1;
+	while (split[++i])
+	{
+		y = -1;
+		dec = 0;
+		nul = 0;
+		while (split[i][++y])
+		{
+			if (split[i][y] == '.')
+				dec++;
+			else if (dec > 0)
+			{
+				if (split[i][y] != '0')
+					nul++;
+			}
+		}
+		if (dec > 1 || nul != 0)
+			return (1);
+	}
+	return (0);
+}
 
 int	parse_coordinates(t_v3 *point, char *line)
 {
@@ -38,7 +92,8 @@ int	parse_orientation(t_v3 *point, char *line)
 	point->z = ft_atof(split[2]);
 	if (point->x < -1 || point->x > 1
 		|| point->y < -1 || point->y > 1
-		|| point->z < -1 || point->z > 1)
+		|| point->z < -1 || point->z > 1
+		|| orientation_check(split))
 	{
 		return (cleanup_split(split,
 				"Orientation vector values have to be in range [-1,1]"));
@@ -57,6 +112,8 @@ int	parse_color(t_gf_color *color, char *line)
 	split = ft_split(line, ',');
 	if (splitsize(split) != 3)
 		return (cleanup_split(split, "Color usage: [R,G,B]"));
+	if (rgb_check(split))
+		return (cleanup_split(split, "Wrong RGB component usage"));
 	color->r = ft_atof(split[0]);
 	color->g = ft_atof(split[1]);
 	color->b = ft_atof(split[2]);
